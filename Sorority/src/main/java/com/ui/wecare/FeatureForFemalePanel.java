c1c3eb2ca3b1dba5780e30d7a1c21228bc7be790
Trisha Ghorpade <ghorpade.t@northeastern.edu>
2022-04-26 12:43:36 -0400

    Renamed modules

--- Sorority/src/main/java/com/ui/wecare/FeatureForFemalePanel.java
+++ Sorority/src/main/java/com/ui/wecare/FeatureForFemalePanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.wicare;
+package com.ui.wecare;
 
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
